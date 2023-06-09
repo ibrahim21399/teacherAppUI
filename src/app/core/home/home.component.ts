4b380b828275fdc341cae5cb643f5e2ed7b377df
Mohamed-2000 <mogenaidy@gmail.com>
2023-06-09 19:48:54 +0300

    leaflet map

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -3,7 +3,8 @@ import { AuthService } from 'src/app/services/auth.service';
 import { admin } from 'src/app/Model/admin';
 import { Student } from 'src/app/Model/Student';
 import { Teacher } from 'src/app/Model/Teacher';
-
+import { HttpClient } from '@angular/common/http';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-home',
@@ -12,25 +13,59 @@ import { Teacher } from 'src/app/Model/Teacher';
 
 })
 export class HomeComponent implements OnInit {
- teacher!:Teacher;
- student!:Student;
+  public highestRatedTeachers!: any[];
+  private userLatitude: number= 0 ;
+  private userLongitude: number = 0 ;
 
 
-  constructor(public auth:AuthService) { }
+  constructor(public auth:AuthService, private http: HttpClient) { }
 
-  ngOnInit(): void {
-    if (this.auth.role=="speaker") {
+  ngOnInit() {
+    this.getUserLocation();
+    this.fetchHighestRatedTeachers();
+  }
 
-      
+  getUserLocation() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.userLatitude = position.coords.latitude;
+        this.userLongitude = position.coords.longitude;
+        this.initializeMap();
+      },
+      (error) => {
+        console.error('Error getting user location:', error);
+      }
+    );
+  }
+
+  initializeMap() {
+    const mapElement = document.getElementById('map');
+
+    if (mapElement) {
+      const map = L.map(mapElement).setView([this.userLatitude, this.userLongitude], 10);
+
+      // Add the tile layer (e.g., OpenStreetMap)
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+        maxZoom: 18
+      }).addTo(map);
+
+      // Place a marker at the user's location
+      const userMarker =  L.marker([this.userLatitude, this.userLongitude]).addTo(map);
+      userMarker.bindPopup('Your Location').openPopup();
     }
   }
 
-  editSpeaker(){
+
+  fetchHighestRatedTeachers() {
+    this.http.get<any[]>('your-api-url/teachers/highest-rated')
+      .subscribe(
+        (teachers) => {
+          this.highestRatedTeachers = teachers;
+        },
+        (error) => {
+          console.error('Error fetching highest-rated teachers:', error);
+        }
+      );
   }
-  hideDialog() {
-
-}
-postEditedSpeaker(){
-
-}
 }
