b89d43c620b7e8d68d5a2711b92a83b2d1e5fc45
Taharder <50746611+sahraouifaris@users.noreply.github.com>
2023-06-10 02:07:43 +0200

    xx

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,9 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { SweetalertService } from './general/sweetalert.service';
+import jwtDecode from 'jwt-decode';
+import { Subject } from 'rxjs';
+
 
 
 @Injectable({
@@ -11,6 +14,9 @@ export class AuthService {
   role?: string;
   isloggedin = false;
   email = "";
+ decodedToken:any; 
+ private IsLogin = new Subject<any>();
+
   url = "http://localhost:8080/api/";
 
   constructor(private http: HttpClient,private router:Router,private _sweetalertService: SweetalertService,
@@ -22,19 +28,36 @@ export class AuthService {
     this.http.post<any>(this.url + "login", { email: email, password: password }).subscribe( res => {
       console.log(res);
       localStorage.setItem("jwt_token", res.token)
+
+      this.decodedToken = jwtDecode<any>(res.token);
+      localStorage.setItem("name", this.decodedToken.name);
+
       this.email = email
       this.isloggedin = true;
+      this.role= this.decodedToken.role;
       this.router.navigateByUrl("/")
-      this.role= res.role;
     },error => {
       this._sweetalertService.RunAlert(error.error.message,false);
     })
   }
 logout(){
   this.isloggedin = false;
-  localStorage.removeItem("jwt_token")
+  localStorage.clear();
   this.email =""
 }
+GetName():any{
+return this.decodedToken.name;
+}
+GetRole():any{
+  return this.decodedToken.role;
+}
+sendIsLogin(login:boolean = false){
+  this.IsLogin.next(login)
+}
+getIsLogin(){
+  return this.IsLogin.asObservable();
+}
+
 //  speakrOwnProfInof (){
 //   return this.http.get<Speaker>(this.url+"Speaker/getownprofile")
 
