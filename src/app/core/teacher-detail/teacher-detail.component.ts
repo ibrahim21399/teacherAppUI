9e64e9d653412083baf126f19eabacf6dbc583e0
Moamen Fares <momenfares43@gmail.com>
2023-06-11 00:47:47 +0200

    Enroll with Ids

--- src/app/core/teacher-detail/teacher-detail.component.ts
+++ src/app/core/teacher-detail/teacher-detail.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Teacher } from 'src/app/Model/Teacher';
 import { TeacherService } from 'src/app/services/Teacher/teacher.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-teacher-detail',
   templateUrl: './teacher-detail.component.html',
   styleUrls: ['./teacher-detail.component.css']
 })
-export class TeacherDetailComponent {
+export class TeacherDetailComponent implements OnInit{
   teacher: Teacher |any;
+  studentId: string="";
+  IsEnrollerd:boolean=false;
 
-  constructor(private teacherService:TeacherService,  private activateRoute:ActivatedRoute) { }
+  constructor(private teacherService:TeacherService,  private activateRoute:ActivatedRoute,private authService:AuthService) { }
 
   ngOnInit(): void {
     this.loadTeacherDetails();
@@ -19,14 +22,22 @@ export class TeacherDetailComponent {
 
   loadTeacherDetails() {
     const id = this.activateRoute.snapshot.params['id'];
-    console.log(id)
     this.teacherService.getTeacherById(id).subscribe(response => {
       if (response) {
         this.teacher = response;
-        console.log(this.teacher)
+        this.teacher = this.teacher[0];
+   
+ 
+
       }
     }, error => {
       console.log(error);
     });
+  }
+  Enroll():void{
+let TeacherId=this.activateRoute.snapshot.params['id'];
+const userId = localStorage.getItem('userId');
+console.log(userId);
+console.log(TeacherId);
   }
 }
