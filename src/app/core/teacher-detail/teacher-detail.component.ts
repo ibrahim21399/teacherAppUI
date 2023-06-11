f05bf9f01ac28472b690906888e6f51c04fcd083
Mohamed-2000 <mogenaidy@gmail.com>
2023-06-12 01:59:06 +0300

    rate

--- src/app/core/teacher-detail/teacher-detail.component.ts
+++ src/app/core/teacher-detail/teacher-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit } from '@angular/core';
+import { Component,EventEmitter,OnInit, Output } from '@angular/core';
 import { ActivatedRoute, Route,Router  } from '@angular/router';
 import { Teacher } from 'src/app/Model/Teacher';
 import { TeacherService } from 'src/app/services/Teacher/teacher.service';
@@ -14,13 +14,16 @@ export class TeacherDetailComponent implements OnInit{
   TeacherId: string="";
   StudentId: string|any;
   IsEnrollerd:boolean=false;
+  @Output() ratingSubmitted = new EventEmitter<number>();
 
+  stars: number[] = [0, 0, 0, 0, 0];
+  selectedRating: number=0;
   constructor(private teacherService:TeacherService,  private activateRoute:ActivatedRoute,private authService:AuthService, private router: Router) { }
 
   ngOnInit(): void {
     this.loadTeacherDetails();
     this.TeacherId=this.activateRoute.snapshot.params['id'];
-this.StudentId = localStorage.getItem('userId');
+    this.StudentId = localStorage.getItem('userId');
 
   }
 
@@ -35,7 +38,7 @@ this.StudentId = localStorage.getItem('userId');
         this.teacher = this.teacher[0];
         if (Array.isArray(this.teacher.studentEnrolled) && this.teacher.studentEnrolled.some((student: any) => student._id === this.StudentId)) {
           this.IsEnrollerd = true;
-        }    
+        }
 
       }
     }, error => {
@@ -45,9 +48,22 @@ this.StudentId = localStorage.getItem('userId');
   Enroll():void{
 this.teacherService.Enroll(this.TeacherId,this.StudentId).subscribe(a=>{this.IsEnrollerd=true})
   }
-    SendMessage(): void {
-      this.router.navigate(['/messages', this.StudentId, this.TeacherId]);
-  
+  SendMessage(): void {
+    this.router.navigate(['/messages', this.StudentId, this.TeacherId]);
+  }
 
+  RateTeacher(): void {
+    this.router.navigate(['/RateTeacher', this.StudentId, this.TeacherId]);
+  }
+
+  setRating(rating: number): void {
+    this.selectedRating = rating;
+    this.stars = this.stars.map((star, index) => index < rating ? 1 : 0);
+  }
+
+  submitRating(): void {
+    if (this.selectedRating) {
+      this.ratingSubmitted.emit(this.selectedRating);
+    }
   }
 }
